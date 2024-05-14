b030168439bccab5e137afcd00af1f38dd0025b7
Edzer Dionido <jeddionido26@gmail.com>
2024-05-14 17:20:03 +0800

    update getting the latest query

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/', function() {
 Route::get('/tasks', function()  {
 
     return view('index', [
-        'tasks' => \App\Models\Task::all()
+        'tasks' => Task::latest()->get()
     ]);
 })->name('tasks.index');
 
