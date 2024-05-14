bb88eab0809839efca1033418c93d63e21d8542c
Edzer Dionido <jeddionido26@gmail.com>
2024-05-14 16:48:30 +0800

    add session flashdata message

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,12 @@
 
 
 @section('content')
+
+@if (session()->has('success'))
+<p> {{session('success')}} </p>
+    
+@endif
+
 <a href="{{route('tasks.create')}}">Create a new task</a>
 @forelse ($tasks as $task)
     <p> <a href="{{route('tasks.show', ['task' => $task->id])}}">Task {{$task->id}} -> {{$task->title}}</a></p>
