9cfca8d450df4682706320bcf86c893b1c02d0bf
Edzer Dionido <jeddionido26@gmail.com>
2024-05-20 11:08:09 +0800

    add tailwind.css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title> Tasks </title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <h1>@yield('title')</h1>
 
-    <div>
+
+    <div class="container mx-auto mt-5 mb-2">
+        <h1 class="text-3xl">@yield('title')</h1>
         @yield('content')
     </div>
 </body>
